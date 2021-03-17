b03d913ceaf915b8e66ac40065853c75474a9432
enzoM97 <enzomarecos@gmail.com>
2021-03-17 16:59:33 -0300

    7 segment display added

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-
+import SevenSegmentDisplay from './SevenSegmentDisplay'
 import { Row, Col } from 'react-bootstrap'
 
 const Clock = () => {
+  const ledSize = {
+    width: '70px',
+    height: '100px'
+  }
   return (
     <Row>
       <Col xs={9} sm={6} md={4} id="clock">
-        <span className="min">10</span>
-        <span className="colon">:</span>
-        <span className="sec">00</span>
+        <Row>
+          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={1} />
+          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
+          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={':'} />
+          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
+          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
+        </Row>
       </Col>
     </Row>
   )
--- components/ShotClock.jsx
+++ components/ShotClock.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import SevenSegmentDisplay from './SevenSegmentDisplay'
 
 const ShotClock = () => {
+  const ledSize = {
+    width: '45px',
+    height: '70px'
+  }
   return (
-    <div id="shotClock">24</div>
+    <div id="shotClock">
+      <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'red'} ledColorOff={'black'} character={2} />
+      <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'red'} ledColorOff={'black'} character={4} />
+    </div>
   )
 }
 
