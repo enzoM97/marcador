5f347d30cf169bf8e2708eb62fa632b25a4dd2a0
enzoM97 <enzomarecos@gmail.com>
2021-04-24 21:08:00 -0400

    leds responsiveness

--- components/Clock.jsx
+++ components/Clock.jsx
@@ -3,18 +3,14 @@ import SevenSegmentDisplay from './SevenSegmentDisplay'
 import { Row, Col } from 'react-bootstrap'
 
 const Clock = () => {
-  const ledSize = {
-    width: '70px',
-    height: '100px'
-  }
   return (
     <Row>
       <Col xs={9} sm={6} md={4} id="clock">
-          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={1} />
-          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
-          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={':'} />
-          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
-          <SevenSegmentDisplay style={ledSize} ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
+          <SevenSegmentDisplay ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={1} />
+          <SevenSegmentDisplay ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
+          <SevenSegmentDisplay ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={':'} />
+          <SevenSegmentDisplay ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
+          <SevenSegmentDisplay ledBorder={'transparent'} ledColorOn={'green'} ledColorOff={'black'} character={0} />
       </Col>
     </Row>
   )
